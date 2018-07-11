698e52b017634ab516b7abe0bac5b728454d64d8
araki2410 <araki2410@gmail.com>
2018-07-11 17:45:15 +0900

    can play tictacto

--- Works/tictac.rb
+++ Works/tictac.rb
@@ -1,28 +1,37 @@
 #!/usr/bin/ruby
 # -*- coding:utf-8 -*-
-
-class Board
-  def initialize(line=3)
-    @board = []
-    cal = Array.new(line,0)
-    line.times do
-      @board.push(cal)
-    end
-  end
-
-  def put_to_stone(x,y,num)
-    print @board[y]
-    if @board[y][x] == 0
-      @board[y][x] = num
-    end
-    print @board[y]
-  end
-end
-
-
+require './board'
 
 hoge = Board.new()
-hoge.put_to_stone(0,0,1)
+player = 2
+turn = 0
 
-#puts hoge.board
+while true
+  if turn > 9
+    break
+  end
+  
+  whos_turn = turn%player + 1
+  print "input x,y:"
+  tag = gets.chomp!
 
+  if /(\d)[\S+|\s+](\d)/ =~ tag
+    x,y = $1.to_i, $2.to_i
+  else
+    print "bad input \n"
+    redo
+  end
+  
+  if hoge.put_to_stone(x-1,y-1,whos_turn) != 0
+    print "bad hoge \n"
+    turn -= 1
+  end
+  hoge.show(1)
+  
+  if hoge.line_check() == 0
+    turn += 1
+  else
+    puts whos_turn
+    break
+  end
+end

1f56a82caa50a95b2079dbc17d04a4af1bc37ff8
araki2410 <38194810+araki2410@users.noreply.github.com>
2018-07-11 20:55:41 +0900

    prac class

---
+++ Works/tictac.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/ruby
+# -*- coding:utf-8 -*-
+
+class Board
+  def initialize(line=3)
+    @board = []
+    cal = Array.new(line,0)
+    line.times do
+      @board.push(cal)
+    end
+  end
+
+  def put_to_stone(x,y,num)
+    print @board[y]
+    if @board[y][x] == 0
+      @board[y][x] = num
+    end
+    print @board[y]
+  end
+end
+
+
+
+hoge = Board.new()
+hoge.put_to_stone(0,0,1)
+
+#puts hoge.board
+
